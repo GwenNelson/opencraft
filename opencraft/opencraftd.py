48f5887cccc5e74f5fef08148f454d2d5ea74971
Gareth Nelson <gareth@garethnelson.com>
2017-01-09 21:14:30 +0000

    i hate chunk data

--- opencraft/opencraftd.py
+++ opencraft/opencraftd.py
@@ -27,6 +27,7 @@ import logging
 import base_daemon
 
 import smpmap
+import bitstring
 
 from twisted.internet import reactor
 from quarry.net.server import ServerFactory, ServerProtocol
@@ -65,56 +66,57 @@ class PlayerData:
        self.cur_yaw      = 0.0
        self.cur_pitch    = 0.0
        self.pending_tps  = set()
+       self.on_ground    = True
+       self.dirty        = True
    def get_cur_chunk(self):
        chunk_x,rx = divmod(int(floor(self.cur_pos[0])),16)
        chunk_z,rz = divmod(int(floor(self.cur_pos[2])),16)
        return (chunk_x,chunk_z)
+   def update_on_ground(self,on_ground):
+       if on_ground != self.on_ground: self.dirty=True
+       self.on_ground = on_ground
+   def update_pos(self,new_pos):
+       if new_pos[0] != self.cur_pos[0]: self.dirty=True
+       if new_pos[1] != self.cur_pos[1]: self.dirty=True
+       if new_pos[2] != self.cur_pos[2]: self.dirty=True
+       self.cur_pos = new_pos
+   def update_look(self,yaw,pitch):
+       if yaw   != self.cur_yaw:   self.dirty = True
+       if pitch != self.cur_pitch: self.dirty = True
+       self.cur_yaw   = yaw
+       self.cur_pitch = pitch
    def get_entity_id(self):
        if self.ent_id == 0:
           self.ent_id = random.randint(0,MAX_ENTS)
        return self.ent_id
 
 class GameState:
-   def __init__(self,global_mode=GAME_MODE_CREATIVE,global_difficulty=GAME_DIFF_PEACEFUL,spawn_pos=(128.0,63.0,128.0),logger=None):
+   def __init__(self,global_mode=GAME_MODE_CREATIVE,global_difficulty=GAME_DIFF_PEACEFUL,spawn_pos=(16.0,34.0,16.0),logger=None):
        self.global_mode       = global_mode
        self.global_difficulty = global_difficulty
        self.players           = set()
        self.existing_ent_ids  = set()
        self.spawn_pos         = spawn_pos
        self.logger            = logger
-   def setup_map(self,chunk_w=4,chunk_h=4):
-       self.logger.info('Creating map of size %sX%s chunks:',str(chunk_w),str(chunk_h))
-
-       self.logger.info(' [+] Generating blank chunk columns')
-       self.chunk_columns={}
-
-       for x in xrange(chunk_w):
-           for z in xrange(chunk_h):
-               self.chunk_columns[(x,z)] = {}
-               for y in xrange(16):
-                   self.chunk_columns[(x,z)][y] = {}
-                   for rx in xrange(16):
-                       for ry in xrange(16):
-                           for rz in xrange(16):
-                               self.chunk_columns[(x,z)][y][(rx,ry,rz)] = (0,0,[]) # block id, block damage value - air by default
-       
-       
-       
-       self.logger.info(' [+] Generating heightmap for %sX%s blocks',str(chunk_w*16),str(chunk_h*16))
-       height_map = smpmap.gen_heightmap(chunk_w*16,chunk_h*16)
-
-       self.logger.info(' [+] Building dirt for %sX%s blocks',str(chunk_w*16),str(chunk_h*16))
-       max_x = chunk_w*16
-       max_z = chunk_h*16
-       for x in xrange(max_x):
-           for z in xrange(max_z):
-               chunk_x,rx = divmod(x,16)
-               chunk_z,rz = divmod(z,16)
-               for y in xrange(height_map[(x,z)]):
-                   chunk_y,ry = divmod(y,16)
-                   self.chunk_columns[(chunk_x,chunk_z)][chunk_y][(rx,ry,rz)] = (3,0,[]) # dirt
-
-       self.logger.info('Generated map!')
+       self.chunk_columns     = {} # maps chunk columns to chunk 16*16*16 sections
+   def load_chunk(self,x,z):
+       retval = {}
+       for y in xrange(0,16): retval[y] = {}
+       # we just set the bottom 2 chunks as full of dirt and the rest as full of air
+       for rx in xrange(0,16):
+           for ry in xrange(0,16):
+               for rz in xrange(0,16):
+                   for chunk_y in xrange(0,2):
+                       retval[chunk_y][(rx,ry,rz)] = (3,0) # dirt
+                   for chunk_y in xrange(2,16):
+                       retval[chunk_y][(rx,ry,rz)] = (0,0) # air
+       return retval
+   def setup_map(self,chunk_w=2,chunk_h=2):
+       self.logger.info('Loading %sX%s flat chunks:',str(chunk_w),str(chunk_h))
+       for x in xrange(chunk_w+1):
+           for z in xrange(chunk_h+1):
+               self.chunk_columns[(x,z)] = self.load_chunk(x,z)
+       self.logger.info('Chunks loaded!')
    def add_player(self,player):
        """Add a new player to the game state
 
@@ -131,57 +133,29 @@ class GameState:
        self.players.add(player)
        player.cur_pos = self.spawn_pos
 
-def get_section_data():
-    # this is insane
-    fd = open('/dev/urandom','rb')
-    retval = fd.read(4096)
-    fd.close()
-    return retval
-
-def get_light_data():
-    fd = open('/dev/urandom','rb')
-    retval = fd.read(16*16*8)
-    fd.close()
-    return retval
-
 class OpenCraftProtocol(ServerProtocol):
    def pack_pos(self,pos_x,pos_y,pos_z):
        val = (int(pos_x)  & 0x3FFFFFF) << 38
        val |= (int(pos_y) & 0xFFF) << 26
        val |= (int(pos_z) & 0x3FFFFFF)
        return self.buff_type.pack('Q',val)
-   def gen_chunk_data(self,chunk_x,chunk_z):
-       self.logger.info('Generate chunk_data packet for %sX%s',str(chunk_x),str(chunk_z))
-       retval  = ''
-       retval += self.buff_type.pack('i',chunk_x)
-       retval += self.buff_type.pack('i',chunk_z)
-       retval += self.buff_type.pack('?',True)      # ground up
-       retval += self.buff_type.pack_varint(65535)  # mask
-
-       chunk_sections = ''
-       for chunk_y in xrange(16):
-           chunk_sections += self.buff_type.pack('b',13) # 13 bits per block
-           chunk_sections += self.buff_type.pack_varint(0)         # pal length of 0
-           chunk_sections += self.buff_type.pack_varint(4096)      # 4096 bytes for the block data
-           chunk_sections += get_section_data()             # placeholder for block data
-           chunk_sections += get_light_data()               # placeholder for light data
-
-       retval += self.buff_type.pack_varint(len(chunk_sections))
-       retval += chunk_sections
-
-       retval += self.buff_type.pack_varint(0) # no entities
-
-       return retval
    def send_chunk_column(self,chunk_x,chunk_z):
        if not self.factory.chunk_data.has_key((chunk_x,chunk_z)):
-          self.factory.chunk_data[(chunk_x,chunk_z)] = self.gen_chunk_data(chunk_x,chunk_z)
+          self.factory.chunk_data[(chunk_x,chunk_z)] = self.factory.gen_chunk_data(chunk_x,chunk_z)
        self.send_packet("chunk_data",self.factory.chunk_data[(chunk_x,chunk_z)])
+   def packet_player_position(self,buff):
+       new_x     = buff.unpack('d')
+       new_y     = buff.unpack('d')
+       new_z     = buff.unpack('d')
+       on_ground = buff.unpack('?')
+       self.player_data.update_pos((new_x,new_y,new_z))
+       self.player_data.update_on_ground(on_ground)
    def packet_player_look(self,buff):
        new_yaw   = buff.unpack('f')
        new_pitch = buff.unpack('f')
-       try_jump  = buff.unpack('?')
-       self.player_data.cur_yaw   = new_yaw
-       self.player_data.cur_pitch = new_pitch
+       on_ground = buff.unpack('?')
+       self.player_data.update_look(new_yaw,new_pitch)
+       self.player_data.update_on_ground(on_ground)
    def packet_client_settings(self,buff):
        locale         = buff.unpack_string()
        view_distance  = buff.unpack('b')
@@ -196,6 +170,7 @@ class OpenCraftProtocol(ServerProtocol):
        pos_x,pos_y,pos_z = self.player_data.cur_pos
        yaw               = self.player_data.cur_yaw
        pitch             = self.player_data.cur_pitch
+       if tp_id==0: tp_id = random.randint(1000,999999)
        self.send_packet("player_position_and_look",self.buff_type.pack('d',pos_x)+
                                                    self.buff_type.pack('d',pos_y)+
                                                    self.buff_type.pack('d',pos_z)+
@@ -209,19 +184,26 @@ class OpenCraftProtocol(ServerProtocol):
        new_z     = buff.unpack('d')
        new_yaw   = buff.unpack('f')
        new_pitch = buff.unpack('f')
-       try_jump  = buff.unpack('?')
+       on_ground = buff.unpack('?')
        # TODO - do proper physics checks here to prevent cheaters
-       self.player_data.cur_pos   = (new_x,new_y,new_z)
-       self.player_data.cur_yaw   = new_yaw
-       self.player_data.cur_pitch = new_pitch
+       self.player_data.update_pos((new_x,new_y,new_z))
+       self.player_data.update_look(new_yaw,new_pitch)
+       self.player_data.update_on_ground(on_ground)
    def packet_player_position(self,buff):
-       new_x = buff.unpack('d')
-       new_y = buff.unpack('d')
-       new_z = buff.unpack('d')
-       try_jump = buff.unpack('?')
-       self.player_data.cur_pos = (new_x,new_y,new_z)
+       new_x     = buff.unpack('d')
+       new_y     = buff.unpack('d')
+       new_z     = buff.unpack('d')
+       on_ground = buff.unpack('?')
+       self.player_data.update_pos((new_x,new_y,new_z))
+       self.player_data.update_on_ground(on_ground)
    def packet_teleport_confirm(self,buff):
        self.player_data.pending_tps.discard(buff.unpack_varint())
+   def client_tick(self):
+       if self.player_data.dirty:
+          self.send_poslook()
+          self.player_data.dirty = False
+       else:
+          self.send_packet("entity",self.buff_type.pack_varint(self.player_data.get_entity_id()))
    def player_joined(self):
        ServerProtocol.player_joined(self)
        self.factory.logger.info('%s has joined' % self.display_name)
@@ -240,10 +222,10 @@ class OpenCraftProtocol(ServerProtocol):
                                     self.buff_type.pack('?',True))
        self.send_packet("spawn_position",self.pack_pos(*self.factory.game_state.spawn_pos))
        start_chunk = self.player_data.get_cur_chunk()
-       for x in xrange(start_chunk[0]-1,start_chunk[0]+2):
-           for y in xrange(start_chunk[1]-1,start_chunk[1]+2):
-               self.send_chunk_column(x,y)
+       self.logger.info('Spawn chunk is %s',str(start_chunk))
+       self.send_chunk_column(*start_chunk)
        self.send_poslook(tp_id=random.randint(1000,999999))
+       self.tasks.add_loop(1.0/20.0,self.client_tick)
 
 class OpenCraftFactory(ServerFactory):
    protocol    = OpenCraftProtocol
@@ -251,6 +233,34 @@ class OpenCraftFactory(ServerFactory):
    online_mode = False
    log_level   = logging.DEBUG
    chunk_data  = {}
+   def gen_chunk_data(self,chunk_x,chunk_z):
+       self.logger.info('Generate chunk_data packet for %sX%s',str(chunk_x),str(chunk_z))
+       retval  = ''
+       retval += self.buff_type.pack('i',chunk_x)
+       retval += self.buff_type.pack('i',chunk_z)
+       retval += self.buff_type.pack('?',True)
+       retval += self.buff_type.pack('i',0x0000ffff) # mask
+
+       sections = ''
+       for chunk_y in xrange(16):
+           sections += self.buff_type.pack('b',13) # bits per entry
+           sections += self.buff_type.pack('i',0)  # pal length == 0
+
+           section_data = ''
+           temp_long    = 0
+           section_block_data = self.game_state.chunk_columns[(chunk_x,chunk_z)][chunk_y]
+           section_bits = bitstring.BitArray()
+           for ry in xrange(16):
+               for rz in xrange(16):
+                   for rx in xrange(16):
+                       section_bits.append(bitstring.BitArray(uint=section_block_data[(rx,ry,rz)][0],length=9))
+                       section_bits.append(bitstring.BitArray(uint=section_block_data[(rx,ry,rz)][1],length=3))
+           section_data = section_bits.bytes
+           sections += self.buff_type.pack('i',len(section_data))
+           sections += section_data
+       retval += self.buff_type.pack('i',len(sections))
+       retval += sections
+       return retval
 
 class OpenCraftServer(base_daemon.BaseDaemon):
    def run(self):
@@ -259,6 +269,9 @@ class OpenCraftServer(base_daemon.BaseDaemon):
        proto_factory.logger     = self.logger
        proto_factory.game_state = GameState(logger=self.logger)
        proto_factory.game_state.setup_map()
+       for k in proto_factory.game_state.chunk_columns.keys():
+           proto_factory.chunk_data[k] = proto_factory.gen_chunk_data(*k)
+       self.logger.info('Server ready!')
        proto_factory.listen('0.0.0.0',25565)
        reactor.run()
 
