f2e58fdcb2dfaa6ce883a511caae97f59617dc1e
Gareth Nelson <gareth@garethnelson.com>
2017-02-22 05:45:49 +0000

    first skeleton of ingame state

---
+++ newview/include/opencraft_appstate_ingame.h
@@ -0,0 +1,95 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2017 by Gareth Nelson (gareth@garethnelson.com)
+//
+// This file is part of the OpenCraft client.
+//
+// The OpenCraft client is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The OpenCraft client is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenCraft.  If not, see <http://www.gnu.org/licenses/>.
+//
+// DESCRIPTION:
+//     ingame appstate
+//
+//-----------------------------------------------------------------------------
+
+#include <common.h>
+#include <SDL.h>
+#include <opencraft_appstate.h>
+
+#include <string>
+#include <vector>
+#include <tuple>
+#include <map>
+
+// loading = loading assets, connecting = duh, playing = duh, dead = dead and awaiting respawn
+
+#define INGAME_LOADING    0
+#define INGAME_CONNECTING 1
+#define INGAME_PLAYING    2
+#define INGAME_DEAD       3
+
+typedef std::tuple<std::string, unsigned int> pending_blockload_t; // texture path, block ID
+typedef std::vector<pending_blockload_t> pending_blockloads_t;
+
+class opencraft_appstate_ingame : public opencraft_appstate{
+   public:
+     opencraft_appstate_ingame(std::string server_addr);
+     ~opencraft_appstate_ingame();
+     void update_state(SDL_Event *ev);
+     void update_loading();
+     void render();
+     int  cur_state; // overall state: loading/connecting/play/dead etc
+   
+     double  progress; // for loading and connecting, meaning should be obvious
+     double  total;
+   private:
+     void load_stuff();
+
+     void update_loading(SDL_Event *ev);
+     void update_connecting(SDL_Event *ev);
+     void update_playing(SDL_Event *ev);
+     void update_dead(SDL_Event *ev);
+     void render_loading();
+     void render_connecting();
+     void render_playing();
+     void render_dead();
+     std::string _server_addr;
+     void add_blockload(std::string texture_path, unsigned int tex_id); // add a block that needs loading
+     void add_blocks(); // call add_blockload for all the blocks
+     std::map<unsigned int,unsigned int> block_tex_id;
+
+     pending_blockloads_t pending_loads;
+
+     unsigned int loading_tex_id;
+     float        loading_x;
+     float        loading_y;
+     int        loading_w;
+     int        loading_h;
+     int        loading_text_w;
+     int        loading_text_h;
+
+     unsigned int dirtblock_gl_tex_id;
+     unsigned int grassblock_gl_tex_id;
+
+
+     float bg_dirt_x;
+     float bg_dirt_y;
+     float bg_dirt_w;
+     float bg_dirt_h;
+
+     float bg_grass_x;
+     float bg_grass_y;
+     float bg_grass_w;
+     float bg_grass_h;     
+
+};
---
+++ newview/src/opencraft_appstate_ingame.cpp
@@ -0,0 +1,153 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2017 by Gareth Nelson (gareth@garethnelson.com)
+//
+// This file is part of the OpenCraft client.
+//
+// The OpenCraft client is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The OpenCraft client is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenCraft.  If not, see <http://www.gnu.org/licenses/>.
+//
+// DESCRIPTION:
+//     ingame appstate
+//
+//-----------------------------------------------------------------------------
+
+#include <common.h>
+#include <opencraft_appstate_ingame.h>
+#include <opencraft_video.h>
+#include <r_2d.h>
+
+extern opencraft_video *oc_video;
+extern void* default_font;
+
+opencraft_appstate_ingame::opencraft_appstate_ingame(std::string server_addr) {
+    this->cur_state    = INGAME_LOADING;
+    this->total        = 0.0;
+    this->progress     = 0.0;
+    this->_server_addr = server_addr;
+
+    oc_video->enter_2d(); // must do this again if we go back from actual play
+    this->load_stuff(); // stuff that must be loaded before we can render even the loading page
+    this->add_blocks();
+}
+
+void opencraft_appstate_ingame::load_stuff() {
+     LOG(debug) << "load_stuff() for ingame";
+
+     unsigned int tex_id;
+     int w;
+     int h;
+     int text_w;
+     int text_h;
+     predraw_text(default_font,255,255,255,(char*)"Loading stuff",&w,&h,&text_w,&text_h,&tex_id);
+
+     this->loading_w      = w;
+     this->loading_h      = h;
+     this->loading_text_w = text_w;
+     this->loading_text_h = text_h;
+     this->loading_tex_id = tex_id;
+     this->loading_x = (oc_video->res_w/2) - (this->loading_text_w/2);
+     this->loading_y = 10.0f;
+
+
+     this->dirtblock_gl_tex_id  = load_texture(std::string("blocks/dirt.png"));
+     this->grassblock_gl_tex_id = load_texture(std::string("blocks/grass_side.png"));
+
+     this->bg_dirt_x = 0.0f;
+     this->bg_dirt_y = BLOCK_TEXTURE_SIZE;
+     this->bg_dirt_w = oc_video->res_w;
+     this->bg_dirt_h = oc_video->res_h-BLOCK_TEXTURE_SIZE;
+
+     this->bg_grass_x = 0.0f;
+     this->bg_grass_y = 0.0f;
+     this->bg_grass_w = oc_video->res_w;
+     this->bg_grass_h = BLOCK_TEXTURE_SIZE;
+
+}
+
+void opencraft_appstate_ingame::update_state(SDL_Event *ev) {
+     switch(this->cur_state) {
+        case INGAME_LOADING: {
+             this->update_loading(ev);
+        break;}
+        case INGAME_CONNECTING: {
+             this->update_connecting(ev);
+        break;}
+        case INGAME_PLAYING: {
+             this->update_playing(ev);
+        break;}
+        case INGAME_DEAD: {
+             this->update_dead(ev);
+        break;}
+     }
+}
+
+void opencraft_appstate_ingame::add_blockload(std::string texture_path, unsigned int tex_id) {
+     pending_blockload_t new_blockload(texture_path, tex_id);
+     this->pending_loads.push_back(new_blockload);
+}
+
+void opencraft_appstate_ingame::add_blocks() {
+}
+
+void opencraft_appstate_ingame::update_loading(SDL_Event *ev) {
+     // TODO - load the blocks here
+}
+
+void opencraft_appstate_ingame::update_connecting(SDL_Event *ev) {
+}
+
+void opencraft_appstate_ingame::update_playing(SDL_Event *ev) {
+}
+
+void opencraft_appstate_ingame::update_dead(SDL_Event *ev) {
+}
+
+void opencraft_appstate_ingame::render_loading() {
+     draw_tiled_quad(this->bg_dirt_x,  this->bg_dirt_y,  this->bg_dirt_w,  this->bg_dirt_h,  BLOCK_TEXTURE_SIZE, BLOCK_TEXTURE_SIZE, this->dirtblock_gl_tex_id);
+     draw_tiled_quad(this->bg_grass_x, this->bg_grass_y, this->bg_grass_w, this->bg_grass_h, BLOCK_TEXTURE_SIZE, BLOCK_TEXTURE_SIZE, this->grassblock_gl_tex_id);
+     glEnable(GL_BLEND);
+      draw_textured_quad(this->loading_x,    this->loading_y,    this->loading_w,    this->loading_h,    this->loading_tex_id);
+     glDisable(GL_BLEND);
+}
+
+void opencraft_appstate_ingame::render_connecting() {
+}
+
+void opencraft_appstate_ingame::render_playing() {
+}
+
+void opencraft_appstate_ingame::render_dead() {
+}
+
+void opencraft_appstate_ingame::render() {
+     switch(this->cur_state) {
+        case INGAME_LOADING: {
+             this->render_loading();
+        break;}
+        case INGAME_CONNECTING: {
+             this->render_connecting();
+        break;}
+        case INGAME_PLAYING: {
+             this->render_playing();
+        break;}
+        case INGAME_DEAD: {
+             this->render_dead();
+        break;}
+     }
+}
+
+opencraft_appstate_ingame::~opencraft_appstate_ingame() {
+    // TODO - unload textures and crap here
+    // TODO - return back to main menu
+}
