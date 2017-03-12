d3d26371036413fa6e1171e8ac2ade4679b6932d
Gareth Nelson <gareth@garethnelson.com>
2017-03-12 12:00:12 +0000

    VERY VERY empty skeleton code

---
+++ launcher/src/main.cpp
@@ -0,0 +1,43 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2017 by Gareth Nelson (gareth@garethnelson.com)
+//
+// This file is part of the OpenCraft launcher.
+//
+// The OpenCraft client is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The OpenCraft launcher is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenCraft.  If not, see <http://www.gnu.org/licenses/>.
+//
+// DESCRIPTION:
+//          Main entry point
+//
+//-----------------------------------------------------------------------------
+#include <iostream>
+#include <string>
+
+#include <common.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <physfs.h>
+#include <SDL.h>
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char **argv) {
+    int i;
+    cout << OPENCRAFT_LAUNCHER_LONG_VER << endl << "Built on " << OPENCRAFT_LAUNCHER_BUILDDATE << endl;
+    
+}
