eb899b8524446b910dcb4cffe29d11f8fdade0e0
Gareth Nelson <gareth@garethnelson.com>
2017-03-12 12:25:54 +0000

    import nuklear

--- launcher/include/nuklear_cpp.h
+++ launcher/include/nuklear_cpp.h
@@ -2,14 +2,14 @@
 //
 // Copyright (C) 2017 by Gareth Nelson (gareth@garethnelson.com)
 //
-// This file is part of the OpenCraft launcher.
+// This file is part of the OpenCraft client.
 //
 // The OpenCraft client is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 //
-// The OpenCraft launcher is distributed in the hope that it will be useful,
+// The OpenCraft client is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -18,27 +18,14 @@
 // along with OpenCraft.  If not, see <http://www.gnu.org/licenses/>.
 //
 // DESCRIPTION:
-//          Main entry point
+//     Header file for nuklear.cpp
 //
 //-----------------------------------------------------------------------------
-#include <iostream>
-#include <string>
+
+#pragma once
 
 #include <common.h>
+#include <nuklear.h>
+#include <nuklear_sdl_gl3.h>
 
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <nuklear_cpp.h>
-
-using std::cerr;
-using std::cout;
-using std::endl;
-using std::string;
-
-int main(int argc, char **argv) {
-    int i;
-    cout << OPENCRAFT_LAUNCHER_LONG_VER << endl << "Built on " << OPENCRAFT_LAUNCHER_BUILDDATE << endl;
-   
-    init_nuklear("OpenCraft Launcher ");
-}
+void init_nuklear(const char* wintitle);
