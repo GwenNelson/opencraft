6b20974688308260647e76dfb89b7f890a1cceb8
Gareth Nelson <gareth@garethnelson.com>
2017-03-13 23:17:17 +0000

    begin implementing plugin system

---
+++ launcher/include/plugin_api.h
@@ -0,0 +1,37 @@
+//-----------------------------------------------------------------------------
+//
+// Copyright (C) 2017 by Gareth Nelson (gareth@garethnelson.com)
+//
+// This file is part of the OpenCraft client.
+//
+// The OpenCraft client is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The OpenCraft client is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenCraft.  If not, see <http://www.gnu.org/licenses/>.
+//
+// DESCRIPTION:
+//     Used to compile plugins
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include <version.h>
+
+typedef enum {MODTYPE_CLIENT,MODTYPE_SERVER} module_type_t;
+
+typedef struct {
+   module_type_t module_type;
+   const char*   module_name;
+   const char*   module_author;
+   const char*   module_copyright;
+   const char*   module_license;
+} module_info_t;
