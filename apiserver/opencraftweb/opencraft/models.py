dfb49b31ed2eda7e2366c7a7073421ac0b615df3
Gareth Nelson <gareth@garethnelson.com>
2017-04-01 05:19:49 +0100

    finish off DB models

--- apiserver/opencraftweb/opencraft/models.py
+++ apiserver/opencraftweb/opencraft/models.py
@@ -13,3 +13,22 @@ class User(models.Model):
    UserRegistered       = models.DateTimeField(auto_now_add=True)
    UserPasswordChange   = models.DateTimeField(default=datetime.now)
    UserPasswordHash     = models.CharField(max_length=60,default="")
+
+class UserProfile(models.Model):
+   ProfileID            = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+   UserID               = models.ForeignKey(User, on_delete=models.CASCADE)
+   UserPlayerName       = models.CharField(max_length=16)
+   UserSkinURL          = models.URLField()
+
+class UserProperty(models.Model):
+   UserID               = models.ForeignKey(User, on_delete=models.CASCADE)
+   PropertyName         = models.CharField(max_length=30)
+   PropertyValue        = models.TextField()
+
+class GameSession(models.Model):
+   SessionID            = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+   UserID               = models.ForeignKey(User, on_delete=models.CASCADE)
+   ProfileID            = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+   ClientToken          = models.CharField(max_length=36)
+   AccessToken          = models.CharField(max_length=36)
+   LastAccess           = models.DateTimeField(auto_now=True)
